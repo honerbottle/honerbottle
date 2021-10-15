388adcd34eabbe9751f1f21aef4e2dbbf624ed2f
qudwnbj <78632744+qudwnbj@users.noreply.github.com>
2021-10-15 18:20:10 +0900

    Updates

---
+++ static/js/main.d97ed29d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonphonor-bottle-portfolio"]=this["webpackJsonphonor-bottle-portfolio"]||[]).push([[0],{9:function(o,t,e){"use strict";e.r(t);e(0);var n=e(3),r=e.n(n),s=e(1);var c=function(){return Object(s.jsx)("div",{className:"App"})};r.a.render(Object(s.jsx)(c,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.d97ed29d.chunk.js.map
