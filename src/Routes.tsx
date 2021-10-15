e81402b99daedbdf90255a40933fa699ff8fd468
qudwnbj <78632744+qudwnbj@users.noreply.github.com>
2021-10-15 18:57:37 +0900

    setUp (1)

---
+++ src/Routes.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface RoutesProps {}
+
+const Routes: React.FC<RoutesProps> = () => {
+  return <div>Routes</div>;
+};
+
+export default Routes;
---
+++ src/styles/styled.d.ts
@@ -0,0 +1,47 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    defaultAccent: string;
+    background: string;
+    backgroundAlt: string;
+    contrastBorder: string;
+    sidebarForeground: string;
+    scrollbars: string;
+    comments: string;
+    caret: string;
+    findHighlight: string;
+    foreground: string;
+    focusBorder: string;
+    guides: string;
+    lineNumbers: string;
+    invisibles: string;
+    lineHighlight: string;
+    selection: string;
+    shadow: string;
+    inputBackground: string;
+    inputForeground: string;
+    inputBorder: string;
+    scrollbarsHover: string;
+    statusbarForeground: string;
+    listHoverForeground: string;
+    tabActiveForeground: string;
+    inactiveSelectionBackground: string;
+    findMatchBackground: string;
+    findMatchHighlightBackground: string;
+    findMatchHighlightBorder: string;
+    white: string;
+    black: string;
+    red: string;
+    orange: string;
+    yellow: string;
+    green: string;
+    cyan: string;
+    blue: string;
+    paleblue: string;
+    purple: string;
+    brown: string;
+    pink: string;
+    violet: string;
+  }
+}
