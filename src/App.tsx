35de0a411c2a82a1d040a1f8652afeba16dd83e7
qudwnbj <78632744+qudwnbj@users.noreply.github.com>
2021-10-21 13:43:59 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import Routes from "./Routes";
+import GlobalStyle from "./styles/global-styles";
+import { theme } from "./styles/theme";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Routes />
+    </ThemeProvider>
+  );
 }
 
 export default App;
